449c0c1924f0ffbd46d4863cbec741ddad6fb657
Anthony K. D. T <it25103981@my.sliit.lk>
2026-05-18 23:01:15 +0530

    rider management crud

--- src/main/java/com/se1020/fooddelivery/controller/DeliveryController.java
+++ src/main/java/com/se1020/fooddelivery/controller/DeliveryController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+
+//rider management
 @Controller
 @RequestMapping("/rider")
 public class DeliveryController {
